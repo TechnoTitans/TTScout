74b5faa5f12818891d4475710b3d1887ba8cc6c9
MqxS <maxgoog06@outlook.com>
2023-03-06 22:13:00 -0500

    Working UI

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -6,7 +6,8 @@ from PIL import Image, ImageTk
 class CameraApp:
     def __init__(self, window):
         self.window = window
-        self.window.title("Camera App")
+        self.window.title("TTScout")
+        self.window.resizable(width=False, height=False)
 
         self.video_frame = tk.Frame(self.window)
         self.video_frame.pack(side=tk.TOP, padx=10, pady=10)
@@ -14,16 +15,18 @@ class CameraApp:
         self.snapshot_button = tk.Button(self.window, text="Take Snapshot", command=self.take_snapshot)
         self.snapshot_button.pack(side=tk.BOTTOM, padx=10, pady=10)
 
-        self.cap = cv2.VideoCapture(0)
+        try:
+            self.cap = cv2.VideoCapture(0)
+        except:
+            print("Failed to find camera feed.")
+            exit(1)
 
         self.video_stream = tk.Label(self.video_frame)
         self.video_stream.pack()
 
         self.preview_window = tk.Toplevel(self.window)
+        self.preview_window.resizable(width=False, height=False)
         self.preview_window.withdraw()
-
-        self.preview_label = tk.Label(self.preview_window)
-        self.preview_label.pack()
 
     def update_stream(self):
         ret, frame = self.cap.read()
@@ -40,34 +43,40 @@ class CameraApp:
 
         self.video_stream.after(10, self.update_stream)
 
+    def clear_preview(self):
+        for widget in self.preview_window.winfo_children():
+            widget.destroy()
+
     def take_snapshot(self):
         ret, frame = self.cap.read()
 
         if ret:
             self.snapshot_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            self.clear_preview()
             self.show_preview()
 
     def show_preview(self):
+        self.preview_window.title("Preview")
+
         image = Image.fromarray(self.snapshot_frame)
         image = image.resize((320, 240), Image.LANCZOS)
 
         photo = ImageTk.PhotoImage(image)
 
-        self.preview_label.config(image="")
-        self.preview_label.config(image=photo)
-        self.preview_label.image = photo
-
         self.preview_window.deiconify()
+        preview_label = tk.Label(self.preview_window, image=photo)
+        preview_label.image = photo
+        preview_label.pack()
+
         confirm_button = tk.Button(self.preview_window, text="Confirm", command=self.close_preview)
         confirm_button.pack(side=tk.LEFT)
 
         retry_button = tk.Button(self.preview_window, text="Retry", command=self.retry_preview)
         retry_button.pack(side=tk.RIGHT)
 
-        self.update_stream()
-
     def close_preview(self):
         self.preview_window.withdraw()
+        self.snapshot_frame = None
 
     def retry_preview(self):
         self.preview_window.withdraw()
